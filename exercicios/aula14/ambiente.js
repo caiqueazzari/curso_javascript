8317f47ca88f654c439d830fb2ef0a651d747eac
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-05-17 18:07:00 -0300

    o.o

---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,24 @@
+//var c = 1
+console.log('Vai começar!!')
+for (var c = 1; c <= 5; c++) {
+    console.log(c)
+}
+console.log('Fim')
+
+
+
+
+
+
+/*
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
+*/
+/*
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+*/
