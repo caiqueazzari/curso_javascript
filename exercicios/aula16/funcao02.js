de8366620d2aa456c36fb47cb407a93746e81e2d
Caique Azzari <64171008+caiqueazzari@users.noreply.github.com>
2021-05-20 16:29:01 -0300

    Create

---
+++ exercicios/aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) {
+    return `A soma de ${n1} e ${n2} é de ${n1 + n2}`
+}
+
+console.log(soma(6))
---
+++ exercicios/aula16/funcao03.js
@@ -0,0 +1,6 @@
+
+let v = function(x) { //Função dentro de uma variável
+    return x*2
+}
+
+console.log(v(5))
